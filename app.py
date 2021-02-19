50de4af1e29a8b8b0cdd3c4043637161230f0b2f
alex <5299617+aeeilllmrx@users.noreply.github.com>
2021-02-19 16:32:26 -0500

    update folium map

--- app.py
+++ app.py
@@ -7,9 +7,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     start_coords = (40.6782, -73.9442)
-    folium_map = folium.Map(location=start_coords, zoom_start=14)
-    return folium_map._repr_html_()
+    map = folium.Map(location=start_coords, zoom_start=13)
+
+    locations = [
+        {'lat': -73.96307, 'lon': 40.67626, 'name': 'Bar Meridian', 'heat_type': 'leg lamp'},
+        {'lat': -73.9628, 'lon': 40.68683, 'name': 'Izzy Rose', 'heat_type': 'fire pit'}
+    ]
+
+    for loc in locations:
+        html = '{}<br>{}'.format(loc['name'], loc['heat_type'])
+        iframe = folium.IFrame(html, width=100, height=100)
+        folium.Marker(
+            [loc['lon'], loc['lat']], 
+            popup=folium.Popup(iframe)
+        ).add_to(map)
+
+    return map._repr_html_()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
