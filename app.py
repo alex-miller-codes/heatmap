ac31ad304a86abdf4ef6b7827e5d97511a6c8f93
alex <5299617+aeeilllmrx@users.noreply.github.com>
2021-02-19 15:46:02 -0500

    simple flask folium app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+import folium
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    start_coords = (40.6782, -73.9442)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return folium_map._repr_html_()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
