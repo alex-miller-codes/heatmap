b35268a29d80a7c24782dfaf6c768b85e2fcad54
Alex Miller <alexmiller@Alexs-MacBook-Pro.local>
2024-12-18 12:19:36 -0500

    use clay data

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
 from flask import Flask
 
 import folium
+import pandas as pd
 
 app = Flask(__name__)
+
+def get_locations(file_path):
+    df = pd.read_csv(file_path)
+
+    locations = []
+
+    for _, row in df.iterrows():
+        if pd.notna(row['Latitude']) and pd.notna(row['Longitude']):
+            locations.append(
+                {
+                    'name': row['Name'],
+                    'lat': row['Latitude'],
+                    'lon': row['Longitude'],
+                }
+            )
+
+    return locations
+
 
 @app.route('/')
 def index():
     start_coords = (40.6782, -73.9442)
     map = folium.Map(location=start_coords, zoom_start=13)
 
-    locations = [
-        {'lat': -73.96307, 'lon': 40.67626, 'name': 'Bar Meridian', 'heat_type': 'leg lamp'},
-        {'lat': -73.9628, 'lon': 40.68683, 'name': 'Izzy Rose', 'heat_type': 'fire pit'}
-    ]
+    locations = get_locations('results-clean-filtered.csv')
 
     for loc in locations:
-        html = '{}<br>{}'.format(loc['name'], loc['heat_type'])
+        html = '{}'.format(loc['name'])
         iframe = folium.IFrame(html, width=100, height=100)
         folium.Marker(
-            [loc['lon'], loc['lat']], 
+            [loc['lat'], loc['lon']],
             popup=folium.Popup(iframe)
         ).add_to(map)
 
